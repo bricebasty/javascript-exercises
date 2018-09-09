233e247ad893904cf445f951cc49ecba2e15d68c
BriceBasty <brice.basty1@gmail.com>
2018-09-09 10:38:07 +0200

    Finished tempConversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,12 @@
-const ftoc = function() {
+const ftoc = function (fahrenheit) {
+  return Math.round((((fahrenheit - 32) * 5) / 9) * 10) / 10;
+};
 
-}
-
-const ctof = function() {
-
-}
+const ctof = function (celsius) {
+  return Math.round((celsius * 1.8 + 32) * 10) / 10;
+};
 
 module.exports = {
   ftoc,
-  ctof
-}
+  ctof,
+};
