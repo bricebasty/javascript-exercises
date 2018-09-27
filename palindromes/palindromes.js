cf2e406676bbda017dbe75d2e26ed3f8da87236d
BriceBasty <brice.basty1@gmail.com>
2018-09-27 15:41:42 +0200

    palin first try

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function() {
-
+const palindromes = function(palin) {
+  palin = palin.replace(/[^a-zA-Z0-9]/g).toLowerCase();
+  return (palin.split("").reverse().join("") === palin ? true : false);
 }
 
 module.exports = palindromes
+
+let palin = "Animal loots foliated detail of stool lamina.";
+palin = palin.replace(/[^a-zA-Z0-9]/g).toLowerCase();
+return (palin.split("").reverse().join("") === palin ? true : false);
