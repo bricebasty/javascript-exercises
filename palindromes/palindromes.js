a888696579d5f2e5dcd8ed057df49dd6a2ec206c
BriceBasty <brice.basty1@gmail.com>
2018-09-30 13:36:17 +0200

    solutions

--- palindromes/palindromes.js
+++
@@ -1,6 +0,0 @@
-const palindromes = function(palin) {
-  palin = palin.replace(/[^a-zA-Z0-9]/g).toLowerCase();
-  return (palin.split("").reverse().join("") === palin ? true : false);
-}
-
-module.exports = palindromes
