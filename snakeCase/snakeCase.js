1381fdb542f5e10f718498b571c9711736c10ab8
BriceBasty <brice.basty1@gmail.com>
2018-09-30 14:08:17 +0200

    test solution

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,5 @@
-const snakeCase = function() {
-
+const snakeCase = function(string) {
+  return processedString = string.toLowerCase().replace(/\s/g, "_").replace(/[A-Z]/g, "_");
 }
 
 module.exports = snakeCase
