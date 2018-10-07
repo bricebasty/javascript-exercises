862c0fd13a47f370d1ad341d882689f1b9d2b887
BriceBasty <brice.basty1@gmail.com>
2018-10-07 12:19:34 +0200

    🐛 fix: return processedWord

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,41 +1,20 @@
 function translate(sentence) {
   const vowels = ["a", "e", "i", "o", "u", "y"];
-  const consonants = [
-    "b",
-    "c",
-    "d",
-    "f",
-    "g",
-    "h",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "v",
-    "w",
-    "x",
-    "z"
-  ];
 
   function changeWord(word) {
     let isPigLatin = false;
     let i = 0;
+    let processedWord;
     while (isPigLatin === false) {
       const letter = word.charAt(i);
       if (Array.from(letter).includes(vowels) === false) {
-        word += "ay";
+        processedWord = `${word}ay`;
         isPigLatin = true;
         i += 1;
-      } else {
-        console.log(`${letter} is a conson`);
       }
+      console.log(`${letter} is a conson`);
     }
+    return processedWord;
   }
   sentence.split(" ").forEach(word => {
     changeWord(word);
